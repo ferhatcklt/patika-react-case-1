5b97a8fb1a8edb2a43bc39380db03a5b006cbd0b
ferhat <ferhatcklt@gmail.com>
2022-06-05 02:21:17 +0300

    number parametresi ile gelen veriye göre kullanıcı bilgisi ve kullanıcıya ait postlar listelenmektedir.

--- getPosts.js
+++ getPosts.js
@@ -5,11 +5,11 @@ async function getData(number) {
     "https://jsonplaceholder.typicode.com/users/" + number
   );
   const { data: posts } = await axios(
-    "https://jsonplaceholder.typicode.com/posts/" + number
+    "https://jsonplaceholder.typicode.com/posts?userId=" + number
   );
 
-  console.log("users "+ number, users);
-  console.log("post " + number, posts);
+  console.log("users " + number, users);
+  console.log("post ", posts);
 }
 
 getData(1);
